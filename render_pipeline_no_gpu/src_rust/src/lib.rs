ebc62ff74a877dddaddc4bc1c72339c6854af691
Jens <jaehrig.jens@gmail.com>
2023-11-13 00:49:44 +0100

    comming closer to completing js to rust translation

--- render_pipeline_no_gpu/src_rust/src/lib.rs
+++ render_pipeline_no_gpu/src_rust/src/lib.rs
@@ -1,10 +1,119 @@
-use web_sys::WebGlRenderingContext;
+use cgmath::{Matrix4, Vector3, Rad, SquareMatrix, Matrix, num_traits::PrimInt};
+use web_sys::{WebGlRenderingContext, WebGlUniformLocation};
+use nameof::name_of;
 
 mod mdn;
 
-pub fn main(gl : WebGlRenderingContext) {
+const VERTEX_SHADER_SOURCE : &'static str = r#"
+    attribute vec3 position;
+    attribute vec4 color;
+
+    uniform mat4 model;
+    uniform mat4 projection;
+
+    varying vec4 vColor;
+
+    void main() {
+        vColor = color;
+        gl_Position = projection * model * vec4(position, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE : &'static str  = r#"
+    precision mediump float;
+    varying vec4 vColor;
+
+    void main() {
+        gl_FragColor = vColor;
+        // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+"#;
+
+struct ShaderLocations {
+    pub model : Option<WebGlUniformLocation>,
+    pub projection : Option<WebGlUniformLocation>,
+    pub position : u32,
+    pub color : u32,
+}
+
+pub fn main(gl : &WebGlRenderingContext) {
     println!("Hello, world!");
     let cube = mdn::create_cube_data();
     let buffers = mdn::create_buffers_for_cube(gl.as_ref(), cube);
-    println!("{}", buffers.n_elements);
+    let vert_shader = mdn::create_shader(&gl, &VERTEX_SHADER_SOURCE, WebGlRenderingContext::VERTEX_SHADER);
+    let frag_shader = mdn::create_shader(&gl, &FRAGMENT_SHADER_SOURCE, WebGlRenderingContext::FRAGMENT_SHADER);
+    let prog = mdn::link_program(&gl, &vert_shader, &frag_shader);
+    gl.use_program(Option::Some(prog.as_ref()));
+    gl.enable(WebGlRenderingContext::DEPTH_TEST);
+
+
+    let locations = ShaderLocations{
+        position: gl.get_attrib_location(&prog, name_of!(position in ShaderLocations)) as u32,
+        projection: gl.get_uniform_location(&prog, name_of!(projection in ShaderLocations)),
+        model: gl.get_uniform_location(&prog, name_of!(model in ShaderLocations)),
+        color: gl.get_attrib_location(&prog, name_of!(color in ShaderLocations)) as u32,
+    };
+}
+
+
+struct RenderParams {
+    x: f32,
+    y: f32,
+    z: f32,
+    rot_x: Rad<f32>,
+    rot_y: Rad<f32>,
+    fov: Rad<f32>,
+    near: f32,
+    far: f32,
+}
+
+pub fn draw(gl : &WebGlRenderingContext, state : &RenderParams) {
+    let model = [
+        Matrix4::from_translation(Vector3::new(state.x, state.y, state.z)), // step 4
+        Matrix4::from_angle_y(state.rot_y), // step 3
+        Matrix4::from_angle_x(state.rot_x), // step 2
+        Matrix4::from_scale(5.),                   // step 1
+    ].iter().fold(&cgmath::One::one(), |a, b| &(a * b));
+
+    let projection = cgmath::perspective(state.fov, 1., state.near, state.far);
+
+    // Update the data going to the GPU
+    update_attributes_and_uniforms(&gl, ShaderInput {  
+        model_transform: &model,
+        projection: &projection,
+    });
+
+    // Perform the actual draw
+    gl.draw_elements_with_i32(
+        WebGlRenderingContext::TRIANGLES,
+        this.buffers.elementsCount,
+        WebGlRenderingContext::UNSIGNED_SHORT,
+        0);
+
+    // Run the draw as a loop
+    //requestAnimationFrame(this.draw.bind(this));
+}
+
+struct ShaderInput<'a> {
+    model_transform: &'a Matrix4<f32>,
+    projection: &'a Matrix4<f32>,
+    locations: &'a ShaderLocations,
+}
+
+fn update_attributes_and_uniforms(gl : &WebGlRenderingContext, data : ShaderInput) {
+    // Setup the color uniform that will be shared across all triangles
+    gl.uniform_matrix4fv_with_f32_array(data.locations.model.as_ref(), false, AsRef::<[f32; 16]>::as_ref(&data.model_transform) );
+    gl.uniform_matrix4fv_with_f32_array(data.locations.projection.as_ref(), false, AsRef::<[f32; 16]>::as_ref(&data.projection));
+
+    // Set the positions attribute
+    gl.enable_vertex_attrib_array(data.locations.position);
+    // gl.bindBuffer(WebGlRenderingContext::ARRAY_BUFFER, this.buffers.positions);
+    // gl.vertexAttribPointer(this.handlers.position, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+
+    // // Set the colors attribute
+    gl.enable_vertex_attrib_array(data.locations.color);
+    // gl.bindBuffer(WebGlRenderingContext::ARRAY_BUFFER, this.buffers.colors);
+    // gl.vertexAttribPointer(this.handlers.color, 4, WebGlRenderingContext::FLOAT, false, 0, 0);
+
+    // gl.bindBuffer(WebGlRenderingContext::ELEMENT_ARRAY_BUFFER, this.buffers.elements);
 }
--- render_pipeline_no_gpu/src_rust/src/mdn.rs
+++ render_pipeline_no_gpu/src_rust/src/mdn.rs
@@ -1,4 +1,5 @@
-use web_sys::{WebGlRenderingContext, WebGlBuffer};
+use cgmath::{Matrix3, Angle, Matrix4};
+use web_sys::{WebGlRenderingContext, WebGlBuffer, WebGlShader, WebGlProgram};
 
 pub struct CubeData {
     pub positions: Vec<f32>,
@@ -140,76 +141,59 @@ pub fn create_buffers_for_cube(gl: &WebGlRenderingContext, cube: CubeData) -> Cr
     }
 }
 
+pub fn create_shader(gl : &WebGlRenderingContext, source : &str, type_ : u32) -> WebGlShader {
+    // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
 
-//     pub fn multiplyPoint(matrix : Float16, point : Float4) : Float4 {
-//         var x = point[0], y = point[1], z = point[2], w = point[3];
+    let shader = gl.create_shader(type_).unwrap();
+    gl.shader_source(&shader, source);
+    gl.compile_shader(&shader);
 
-//         var c1r1 = matrix[0], c2r1 = matrix[1], c3r1 = matrix[2], c4r1 = matrix[3],
-//             c1r2 = matrix[4], c2r2 = matrix[5], c3r2 = matrix[6], c4r2 = matrix[7],
-//             c1r3 = matrix[8], c2r3 = matrix[9], c3r3 = matrix[10], c4r3 = matrix[11],
-//             c1r4 = matrix[12], c2r4 = matrix[13], c3r4 = matrix[14], c4r4 = matrix[15];
+    if !gl.get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS) {
+        let info = gl.get_shader_info_log(&shader);
+        panic!("{:?}", info);
+    }
 
-//         return [
-//             x * c1r1 + y * c1r2 + z * c1r3 + w * c1r4,
-//             x * c2r1 + y * c2r2 + z * c2r3 + w * c2r4,
-//             x * c3r1 + y * c3r2 + z * c3r3 + w * c3r4,
-//             x * c4r1 + y * c4r2 + z * c4r3 + w * c4r4
-//         ];
-//     }
+    return shader
+}
 
+pub fn link_program(gl: &WebGlRenderingContext, vertex_shader : &WebGlShader, fragment_shader : &WebGlShader) -> WebGlProgram {
+    let program = gl.create_program().unwrap();
 
-// import { Float16, Float4, nullRefError } from "./common";
+    gl.attach_shader(&program, vertex_shader);
+    gl.attach_shader(&program, fragment_shader);
+
+    gl.link_program(&program);
+
+    if !gl.get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS) {
+        let info = gl.get_program_info_log(&program);
+        panic!("{:?}", info);
+    }
+
+    return program;
+}
+
+pub fn perspective_matrix(fov : f32, aspect_ratio : f32, near : f32, far : f32) -> Matrix4<f32> {
+    // Construct a perspective matrix
+
+    /*
+        Field of view - the angle in radians of what's in view along the Y axis
+        Aspect Ratio - the ratio of the canvas, typically canvas.width / canvas.height
+        Near - Anything before this point in the Z direction gets clipped (outside of the clip space)
+        Far - Anything after this point in the Z direction gets clipped (outside of the clip space)
+    */
+
+    let f = 1.0f32 / (fov / 2f32).tan();
+    let range_inv = 1f32 / (near - far);
+
+    return Matrix4::new(
+        f / aspect_ratio, 0.0, 0.0, 0.0,
+        0.0, f, 0.0, 0.0,
+        0.0, 0.0, (near + far) * range_inv, -1.0,
+        0.0, 0.0, near * far * range_inv * 2.0, 0.0
+    );
+}
 
 // export class MDN {
-
-//     public static multiplyMatrices(a: Float16, b: Float16): Float16 {
-//         // TODO - Simplify for explanation
-//         // currently taken from https://github.com/toji/gl-matrix/blob/master/src/gl-matrix/mat4.js#L306-L337
-
-//         var result: Float16 = new Array<number>(16) as Float16;
-
-//         var a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
-//             a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
-//             a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
-//             a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
-
-//         // Cache only the current line of the second matrix
-//         var b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
-//         result[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-//         result[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-//         result[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-//         result[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-
-//         b0 = b[4]; b1 = b[5]; b2 = b[6]; b3 = b[7];
-//         result[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-//         result[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-//         result[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-//         result[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-
-//         b0 = b[8]; b1 = b[9]; b2 = b[10]; b3 = b[11];
-//         result[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-//         result[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-//         result[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-//         result[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-
-//         b0 = b[12]; b1 = b[13]; b2 = b[14]; b3 = b[15];
-//         result[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-//         result[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-//         result[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-//         result[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-
-//         return result;
-//     }
-
-//     public static multiplyArrayOfMatrices(matrices : Float16[]) : Float16 {
-//         var inputMatrix = matrices[0];
-
-//         for (var i = 1; i < matrices.length; i++) {
-//             inputMatrix = MDN.multiplyMatrices(inputMatrix, matrices[i]);
-//         }
-
-//         return inputMatrix;
-//     }
 
 //     public static rotateXMatrix(a) : Float16  {
 //         var cos = Math.cos;
@@ -265,27 +249,6 @@ pub fn create_buffers_for_cube(gl: &WebGlRenderingContext, cube: CubeData) -> Cr
 //         ];
 //     }
 
-//     public static perspectiveMatrix(fieldOfViewInRadians, aspectRatio, near, far) : Float16 {
-//         // Construct a perspective matrix
-
-//         /*
-//            Field of view - the angle in radians of what's in view along the Y axis
-//            Aspect Ratio - the ratio of the canvas, typically canvas.width / canvas.height
-//            Near - Anything before this point in the Z direction gets clipped (outside of the clip space)
-//            Far - Anything after this point in the Z direction gets clipped (outside of the clip space)
-//         */
-
-//         var f = 1.0 / Math.tan(fieldOfViewInRadians / 2);
-//         var rangeInv = 1 / (near - far);
-
-//         return [
-//             f / aspectRatio, 0, 0, 0,
-//             0, f, 0, 0,
-//             0, 0, (near + far) * rangeInv, -1,
-//             0, 0, near * far * rangeInv * 2, 0
-//         ];
-//     }
-
 //     public static orthographicMatrix(left, right, bottom, top, near, far) : Float16  {
 //         // Each of the parameters represents the plane of the bounding box
 
@@ -303,36 +266,5 @@ pub fn create_buffers_for_cube(gl: &WebGlRenderingContext, cube: CubeData) -> Cr
 //             0, 0, 2 * nf, 0,
 //             row4col1, row4col2, row4col3, 1
 //         ];
-//     }
-
-//     public static createShader(gl, source, type) {
-//         // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
-
-//         var shader = gl.createShader(type);
-//         gl.shaderSource(shader, source);
-//         gl.compileShader(shader);
-
-//         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-//             var info = gl.getShaderInfoLog(shader);
-//             throw "Could not compile WebGL program. \n\n" + info;
-//         }
-
-//         return shader
-//     }
-
-//     public static linkProgram(gl: WebGLRenderingContext, vertexShader, fragmentShader): WebGLProgram {
-//         var program = gl.createProgram() ?? nullRefError();
-
-//         gl.attachShader(program, vertexShader);
-//         gl.attachShader(program, fragmentShader);
-
-//         gl.linkProgram(program);
-
-//         if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-//             var info = gl.getProgramInfoLog(program);
-//             throw "Could not compile WebGL program. \n\n" + info;
-//         }
-
-//         return program;
 //     }
 // }
